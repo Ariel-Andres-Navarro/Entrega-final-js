394367ba747ce54158eeb455188d79cb8061512a
Ariel Navarro <navarroariel1974@gmail.com>
2024-07-07 18:34:46 -0300

    agrege cantidad al array de productos

--- main.js
+++ main.js
@@ -10,63 +10,72 @@ const productos = [
         nombre: "Yacochuya Malbec",
         precio: 51000,
         img: 
-        "https://www.espaciovino.com.ar/media/default/0001/54/thumb_53455_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/54/thumb_53455_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "Sapo de otro Pozo blend ",
         precio: 15200,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/59/thumb_58716_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/59/thumb_58716_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "Alegoría Malbec",
         precio: 13600,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/61/thumb_60441_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/61/thumb_60441_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: "Angelica Zapata Malbec",
         precio: 25130,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/53/thumb_52803_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/53/thumb_52803_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 5,
         nombre: "DV Catena Cabernet Malbec",
         precio: 9380,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/53/thumb_52791_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/53/thumb_52791_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 6,
         nombre: "Familia Gascón Red blend",
         precio: 3805,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/70/thumb_69197_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/70/thumb_69197_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 7,
         nombre: "Frida Malbec",
         precio: 3760,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/67/thumb_66114_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/67/thumb_66114_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 8,
         nombre: "Gauchesco clásico Cabernet Sauvignon",
         precio: 3515,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/70/thumb_69001_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/70/thumb_69001_default_small.jpeg",
+        cantidad: 1,
     },
     {
         id: 9,
         nombre: "Rutini Cabernet Malbec",
         precio: 10800,
         img:
-        "https://www.espaciovino.com.ar/media/default/0001/62/thumb_61998_default_small.jpeg"
+        "https://www.espaciovino.com.ar/media/default/0001/62/thumb_61998_default_small.jpeg",
+        cantidad: 1,
     }
 ];  
 
@@ -145,7 +154,7 @@ productos.forEach((product) => {
 
 
 
-    const total =carrito.reduce((acc, el) => acc + el.precio, 0);
+    const total =carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
 
     const totalBuying = document.createElement("div");
     totalBuying.className = "total-content";
