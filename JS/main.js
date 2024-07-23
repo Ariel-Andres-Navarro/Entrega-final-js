5dc288300c70db954d834f259b11fa3a87260812
Ariel Navarro <navarroariel1974@gmail.com>
2024-07-22 22:25:07 -0300

    modificación de la ubicación de la notificación de toastify

--- JS/main.js
+++ JS/main.js
@@ -62,7 +62,7 @@ Toastify({
     duration: 3000,
     close: true,
     gravity: "top", // `top` or `bottom`
-    position: "rigth", // `left`, `center` or `right`
+    position: "right", // `left`, `center` or `right`
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       background: "linear-gradient(to right, #00b09b, #96c93d)",
